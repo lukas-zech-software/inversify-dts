f91c77e938da4308a660d90b4c09a049953039fa
Remo H. Jansen <remo.jansen@wolksoftware.com>
2016-06-28 23:53:09 +0100

    added inversify-devtools.d.ts

---
+++ inversify-devtools/inversify-devtools.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for inversify 1.0.0-beta.1
+// Project: https://github.com/inversify/inversify-devtools
+// Definitions by: inversify <https://github.com/inversify/>
+
+/// <reference path="../inversify/inversify.d.ts" />
+
+interface Window {
+  __inversifyDevtools__(kernel: inversify.interfaces.Kernel): void;
+}
+
+declare module "inversify-devtools" {
+  declare var render: (container: string) => void;
+  export default render;
+}
